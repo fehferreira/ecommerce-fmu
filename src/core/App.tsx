5e627be05ddb6d0ed7a79bc261bdf92053c57f19
Felipe Silva <felipe.ferreira1003@gmail.com>
2022-07-22 22:45:44 -0300

    feat: include prettier on project

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Router from "./Router";
-import { AppStyled, GlobalStyled } from "./styles";
+import React from 'react';
+import Router from './Router';
+import { AppStyled, GlobalStyled } from './styles';
 
 const App: React.FC = () => (
-  <>
-    <GlobalStyled />
-    <AppStyled>
-      <Router />
-    </AppStyled>
-  </>
+	<>
+		<GlobalStyled />
+		<AppStyled>
+			<Router />
+		</AppStyled>
+	</>
 );
 
 export default App;
